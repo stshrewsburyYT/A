c8455204a735e62041258746963c5f27f271482e
stshrewsburyYT <70076359+stshrewsburyYT@users.noreply.github.com>
2020-08-22 22:03:59 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,12 @@
+import discord, jishaku, os
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="g!")
+bot.remove_command("help")
+bot.load_extension("jishaku")
+
+@bot.event
+async def on_ready():
+    print("yes online now")
+
+bot.run(str(os.environ.get("TOKEN"))
